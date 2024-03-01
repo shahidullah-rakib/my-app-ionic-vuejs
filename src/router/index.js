9a6f3e2f2841195c0b47917da49d31c9e6cb69be
DESKTOP-8TBDQ8N\USER <rakib.ygbl@gmail.com>
2024-03-02 03:22:29 +0600

    practics code

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,15 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import TabsPage from '../views/TabsPage.vue';
+
+import MemoriesPage from '../pages/MemoriesPage.vue';
 
 const routes = [
   {
     path: '/',
-    redirect: '/tabs/tab1',
+    redirect: '/memories',
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1',
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue'),
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue'),
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue'),
-      },
-    ],
+    path: 'memories',
+    component: MemoriesPage,
   },
 ];
 
