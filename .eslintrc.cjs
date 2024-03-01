a0ffec8ebd80196dd637a6eb1ea9170e10d948fd
DESKTOP-8TBDQ8N\USER <rakib.ygbl@gmail.com>
2024-03-02 02:53:51 +0600

    project setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    // removed for ts to js
+    // '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-deprecated-slot-attribute': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  }
-}
+    // removed for ts to js
+    // '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -22,7 +22,15 @@
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+<script setup>
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
 </script>
